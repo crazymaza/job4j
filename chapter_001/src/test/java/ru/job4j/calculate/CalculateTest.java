b033e4814e861512b01652bd7cf4b87a9fe06cd1
Igor Valnev <dx2389@gmail.com>
2018-07-03 23:00:00 +0400

    Сделал автоформатирование файлов

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,22 +5,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Igor Valnev
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Igor Valnev
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
 	/**
-	* Test echo.
-	*/ @Test
+	 * Test echo.
+	 */ @Test
 	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Igor";
-		String expect = "Echo, echo, echo : Igor"; 
+		String expect = "Echo, echo, echo : Igor";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
 	}
- 
+
 }
