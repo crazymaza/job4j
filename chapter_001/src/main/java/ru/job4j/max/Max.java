55203eab4e0ac030641a0ff28a0adbb280a5ae58
Igor Valnev <dx2389@gmail.com>
2018-07-07 00:34:07 +0300

    Добавил перегрузку метода в класс Max. Теперь можем вычислять максимальное из трех чисел.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -14,4 +14,9 @@ public class Max {
     public int max(int first, int second) {
         return first > second ? first : second;
     }
+
+
+    public int max(int first, int second, int third) {
+        return this.max((this.max(first, second)), third);
+    }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -7,10 +7,18 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whoIsBiggest() {
+    public void whichOfTheTwoNumbersIsGreater() {
         Max maximum = new Max();
         int result = maximum.max(1, 2);
         int expected = 2;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whichOfTheThreeNumbersIsGreater() {
+        Max maximum = new Max();
+        int result = maximum.max(1, 2, 3);
+        int expected = 3;
         assertThat(result, is(expected));
     }
 }
