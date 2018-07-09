078709da214c6664aba5f8ebea6a0f5e69b1eb44
Igor Valnev <dx2389@gmail.com>
2018-07-09 20:40:09 +0300

    Написал метод подсчета суммы четных чисел из диапазона.

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void sumOfNumbersFromOneToTen() {
+        Counter counter = new Counter();
+        int result = counter.add(1, 10);
+        int expected = 30;
+        assertThat(result, is(expected));
+    }
+}
