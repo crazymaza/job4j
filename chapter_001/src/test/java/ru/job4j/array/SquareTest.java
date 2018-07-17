16bb28785226c6ad39432001abe786e4707ffa77
Igor Valnev <dx2389@gmail.com>
2018-07-17 20:35:42 +0300

    Create class Square and SquareTest

---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] result = square.calculate(bound);
+        int[] expected = new int[]{1, 4, 9};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenBoundFiveThen1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] result = square.calculate(bound);
+        int[] expected = new int[]{1, 4, 9, 16, 25};
+        assertThat(result, is(expected));
+    }
+}
