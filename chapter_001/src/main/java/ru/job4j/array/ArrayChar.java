0f6715bcc1dffdffc582957bdbdf67eb0b6c0a15
Igor Valnev <dx2389@gmail.com>
2018-08-04 16:04:33 +0300

    Исправил класс ArrayChar.

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -25,6 +25,7 @@ public class ArrayChar {
         for (int i = 0; i < value.length; i++) {
             if (data[i] != value[i]) {
                 result = false;
+                break;
             }
         }
         return result;
