7bd9229b0f3485ea9d64f7334299eb8d60649c3b
Igor Valnev <dx2389@gmail.com>
2018-08-07 23:27:36 +0300

    Added comments to the MatrixCheck class.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,12 +2,21 @@ package ru.job4j.array;
 
 public class MatrixCheck {
 
+    /**
+     * Метод принимает параметром массив булевых значений и смотрит
+     * значения по диагоналям.
+     * @param data - массив булевых значений.
+     * @return - Результат: true - если по диагоналям одинаковые значения,
+     * в противном случае получаем false.
+     */
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
-            if (!(data[0][0] == data[i][i]) || !(data[(data.length - 1) - i][i]
+        for (int i = 0; i < data.length; i++) { //Проходимся по матрице(двумерному массиву).
+            if (!(data[0][0] == data[i][i]) || !(data[(data.length - 1) - i][i] //Так как главная диагональ это
+                    //0:0 1:1 2:2, то сравниваем эти значения. Побочная диагональ это в обратную сторону
+                    //от длины массива.
                     == data[i][(data.length - 1) - i])) {
-                result = false;
+                result = false; //Если один из методов сравнения выдает нам false, то результат false.
             }
         }
         return result;
