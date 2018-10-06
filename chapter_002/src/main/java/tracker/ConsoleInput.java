f734cc4412467debdb4dbad73af12d7f6f6b402d
Igor Valnev <dx2389@gmail.com>
2018-10-06 17:09:30 +0300

    Edit ConsoleInput and StartUI classes.

--- chapter_002/src/main/java/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/tracker/ConsoleInput.java
@@ -2,14 +2,32 @@ package tracker;
 
 import java.util.Scanner;
 
+/**
+ * Класс пользовательского для считывания значений,
+ * которые пользователь ввел в консоль.
+ */
 public class ConsoleInput {
 
+    /**
+     * Метод, который считывает значения, которые пользователь ввел в консоль.
+     * В программе применяется при создании экземпляров класса Item.
+     *
+     * @param question - Вопрос на который пользователь будет вводить ответ в консоль.
+     * @return возвращает пользовательский ответ.
+     */
     public String ask(String question) {
         System.out.println(question);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
+    /**
+     * Метод, который считывает значения, которые пользователь ввел в консоль.
+     * В программе применяется в методе удаления заявки.
+     *
+     * @param question - Вопрос на который пользователь будет вводить ответ в консоль.
+     * @return возвращает пользовательский ответ.
+     */
     public Character answer(String question) {
         System.out.println(question);
         System.out.println("Если да, то нажмите Y, если нет, то любую клавишу.");
--- chapter_002/src/main/java/tracker/StartUI.java
+++ chapter_002/src/main/java/tracker/StartUI.java
@@ -1,13 +1,20 @@
 package tracker;
 
+/**
+ * Класс в котором мы запускаем программу и
+ * в котором происходит вызов всех методов программы.
+ * <p>
+ * {value} ADD - константа, пункт меню, относится к методу добавления заявки.
+ * {value} REPLACE - константа, пункт меню, относится к методу редактировани заявки.
+ * {value} DELETE - константа, пункт меню, относится к методу удаления заявки.
+ * {value} ALL - константа, пункт меню, относится к методу просмотра всех заявок, которые есть в системе.
+ * {value} BY_NAME - константа, пункт меню, относится к методу поиска заявки по названию.
+ * {value} BY_ID - константа, пункт меню, относится к методу поиска заявки идентификатору(id).
+ * {value} EXIT - константа, пункт меню, относится к процессу выхода из приложения.
+ * {value} input - константа, экземпляр класса ConsoleInput. Отвечает за пользовательский ввод.
+ * {value} tracker - константа, экземпляр класса Tracker. Отвечает за запуск методов из класса Tracker.
+ */
 public class StartUI {
-/*    добавление заявок - public Item add(Item item);
-    редактирование заявок - public void replace(String id, Item item);
-    удаление заявок - public void delete(String id);
-    получение списка всех заявок - public Item[] findAll();
-    получение списка по имени - public Item[] findByName(String key);
-    получение заявки по id - public Item findById(String id);*/
-
     private static final String ADD = "1";
     private static final String REPLACE = "2";
     private static final String DELETE = "3";
@@ -18,6 +25,12 @@ public class StartUI {
     private final ConsoleInput input;
     private final Tracker tracker;
 
+    /**
+     * Консруктор класса
+     *
+     * @param input   - параметром передаем экземпляр класса пользовательского ввода.
+     * @param tracker - параметром передаем экземпляр класса логики программы.
+     */
     public StartUI(ConsoleInput input, Tracker tracker) {
         this.input = input;
         this.tracker = tracker;
@@ -25,6 +38,10 @@ public class StartUI {
 
     /**
      * Основной цикл программы.
+     * Запускаем метод показа меню и сравниваем пользовательский ввод со значением констант.
+     * Если с одним из значений совпало, то запускается метод за который отвечает константа.
+     * Цикл работает до тех пор, пока {value} exit не станет равно true, это произойдет после того
+     * как пользователь выберет выход из программы.
      */
     public void init() {
         boolean exit = false;
@@ -32,7 +49,6 @@ public class StartUI {
             this.showMenu();
             String answer = this.input.ask("Введите пункт меню : ");
             if (ADD.equals(answer)) {
-                //добавление заявки вынесено в отдельный метод.
                 this.createItem();
             } else if (REPLACE.equals(answer)) {
                 this.replaceItem();
@@ -66,7 +82,11 @@ public class StartUI {
     }
 
     /**
-     * Реализация пунктов меню.
+     * ================= Реализация пунктов меню. =================
+     * <p>
+     * Метод запускает создание добавление новой заявки.
+     * Мы получаем нужные нам данные от пользователя, создаем экземпляр класса Item
+     * и затем добавляем его в наш Tracker.
      */
     private void createItem() {
         System.out.println("------------ Добавление новой заявки --------------");
@@ -78,6 +98,11 @@ public class StartUI {
         System.out.println();
     }
 
+    /**
+     * Метод запускает редактирование заявки.
+     * Мы получаем нужные нам данные от пользователя, создаем экземпляр класса Item
+     * и затем заменяем им уже имеющуюся заявку.
+     */
     private void replaceItem() {
         System.out.println("------------ Редактирование заявки --------------");
         String id = this.input.ask("Введите идентификатор заявки(id) :");
@@ -89,6 +114,11 @@ public class StartUI {
         System.out.println();
     }
 
+    /**
+     * Метод запускает удаление заявки.
+     * Мы получаем нужные нам данные от пользователя,
+     * находим заявку и удаляем её из Tracker'a.
+     */
     private void deleteItem() {
         System.out.println("------------ Удаление заявки --------------");
         String id = this.input.ask("Введите идентификатор заявки :");
@@ -102,6 +132,10 @@ public class StartUI {
         System.out.println();
     }
 
+    /**
+     * Метод отвечает за показ всех заявок, котрые имеются в базе Tracker'a.
+     * Показываются заявки по единому образцу.
+     */
     private void getAllItems() {
         System.out.println("------------ Все имеющиеся заявки --------------");
         Item[] items = tracker.getAll();
@@ -114,6 +148,11 @@ public class StartUI {
         System.out.println();
     }
 
+    /**
+     * Метод отвечает за нахождение заявки по названию.
+     * Получаем от пользователя необходимые данные, находим по ним все заявки
+     * и выводим их в консоль по единому образцу.
+     */
     private void searchByName() {
         System.out.println("------------ Поиск заявки по названию --------------");
         String name = this.input.ask("Введите название заявки :");
@@ -128,6 +167,11 @@ public class StartUI {
         System.out.println();
     }
 
+    /**
+     * Метод отвечает за нахождение заявки по названию.
+     * Получаем от пользователя необходимые данные, находим по ним заявку
+     * и выводим ее в консоль по образцу.
+     */
     private void searchById() {
         System.out.println("------------ Поиск заявки по идентификатору(id) --------------");
         String id = this.input.ask("Введите идентификатор(id) заявки :");
@@ -139,9 +183,9 @@ public class StartUI {
     }
 
     /**
-     * Метод для запуска программы.
+     * ================= Метод для запуска программы. =================
      *
-     * @param args
+     * @param args - указания, которые можно добавить при запуске.
      */
     public static void main(String[] args) {
         new StartUI(new ConsoleInput(), new Tracker()).init();
