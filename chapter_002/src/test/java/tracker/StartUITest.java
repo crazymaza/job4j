7af406655ef5f2491d43d8bf0098783472c49aaf
Igor Valnev <dx2389@gmail.com>
2018-10-14 23:14:01 +0300

    ДОДЕЛАТЬ! Добавил тесты по выводу в консоль.

--- chapter_002/src/test/java/tracker/StartUITest.java
+++ chapter_002/src/test/java/tracker/StartUITest.java
@@ -1,11 +1,40 @@
 package tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    private PrintStream stdout = System.out;
+    private ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private Tracker tracker = new Tracker();
+    private String ln = System.lineSeparator();
+
+
+    /**
+     * Данный код выполняется перед запуском тестов.
+     */
+    @Before
+    public void loadOutput() {
+        System.out.println("Before tests");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    /**
+     * Данный код выполняется после запуска тестов.
+     */
+    @After
+    public void loadInput() {
+        System.setOut(this.stdout);
+        System.out.println("After tests");
+    }
+
     /**
      * Как работает данный тип теста:
      * Создаём Tracker, затем создаём StubInput с последовательностью действий.
@@ -47,11 +76,92 @@ public class StartUITest {
 
     @Test
     public void whenFindItemById() {
-        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name", "desc"));
         ConsoleInput input = new StubInput(new String[]{"6", item.getId(), "0"});
         new StartUI(input, tracker).init();
-        assertThat(tracker.findById(item.getId()), is(item));
+        assertThat(tracker.findById(item.getId()),
+                is(
+                        new StringBuilder()
+                                .append("Id заявки: ").append(item.getId()).append("\n")
+                                .append("Название заявки: ").append(item.getName()).append("\n")
+                                .append("Описание заявки: ").append(item.getDescription()).append("\n")
+                                .append("-------------").append(ln)
+                                .append(ln)
+                                .toString()
+                )
+        );
     }
+
+    @Test
+    public void whenUserCheckGetAll() {
+        Item item = new Item("aaa", "aaa");
+        tracker.add(item);
+        ConsoleInput input = new StubInput(new String[]{"4", "0"});
+        new StartUI(input, tracker).init();
+        tracker.findById(item.getId());
+        assertThat(new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("Меню:").append(ln)
+                                .append("1. Добавление заявки").append(ln)
+                                .append("2. Редактирование заявки").append(ln)
+                                .append("3. Удаление заявки").append(ln)
+                                .append("4. Получение списка всех заявок").append(ln)
+                                .append("5. Нахождение заявки по названию").append(ln)
+                                .append("6. Нахождение заявки по идентификатору(id)").append(ln)
+                                .append("0. Выход").append(ln)
+                                .append("------------ Все имеющиеся заявки --------------").append(ln)
+                                .append("Id заявки: ").append(item.getId()).append("\n")
+                                .append("Название заявки: ").append(item.getName()).append("\n")
+                                .append("Описание заявки: ").append(item.getDescription()).append("\n")
+                                .append("-------------").append(ln)
+                                .append(ln)
+                                .append("Меню:").append(ln)
+                                .append("1. Добавление заявки").append(ln)
+                                .append("2. Редактирование заявки").append(ln)
+                                .append("3. Удаление заявки").append(ln)
+                                .append("4. Получение списка всех заявок").append(ln)
+                                .append("5. Нахождение заявки по названию").append(ln)
+                                .append("6. Нахождение заявки по идентификатору(id)").append(ln)
+                                .append("0. Выход").append(ln)
+                                .append("Уже уходите? Ну что ж, до скорой встречи!").append(ln)
+                                .toString()
+                )
+
+        );
+    }
+
+    @Test
+    public void whenFindItemByName() {
+        Item item = new Item("aaa", "aaa");
+        tracker.add(item);
+        ConsoleInput input = new StubInput(new String[]{"4", "0"});
+        new StartUI(input, tracker).init();
+        tracker.findById(item.getName());
+        assertThat(new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("Вот что удалось найти по названию ").append(item.getName())
+                                .append(ln)
+                                .append("Id заявки: ").append(item.getId()).append("\n")
+                                .append("Название заявки: ").append(item.getName()).append("\n")
+                                .append("Описание заявки: ").append(item.getDescription()).append("\n")
+                                .append("-------------").append(ln)
+                                .append(ln)
+                                .append("Меню:").append(ln)
+                                .append("1. Добавление заявки").append(ln)
+                                .append("2. Редактирование заявки").append(ln)
+                                .append("3. Удаление заявки").append(ln)
+                                .append("4. Получение списка всех заявок").append(ln)
+                                .append("5. Нахождение заявки по названию").append(ln)
+                                .append("6. Нахождение заявки по идентификатору(id)").append(ln)
+                                .append("0. Выход").append(ln)
+                                .append("Уже уходите? Ну что ж, до скорой встречи!").append(ln)
+                                .toString()
+                )
+
+        );
+    }
+
 
 }
