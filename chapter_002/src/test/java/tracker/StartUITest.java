77280dfc382c0a0c4afa7ea2344adf12d06c5ef1
Igor Valnev <dx2389@gmail.com>
2018-10-15 22:47:25 +0300

    Editing StartUITest class.

--- chapter_002/src/test/java/tracker/StartUITest.java
+++ chapter_002/src/test/java/tracker/StartUITest.java
@@ -43,7 +43,6 @@ public class StartUITest {
      */
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();
         ConsoleInput input = new StubInput(new String[]{"1", "test name", "desc", "0"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll()[0].getName(), is("test name"));
@@ -57,7 +56,6 @@ public class StartUITest {
      */
     @Test
     public void whenUpdateThenTrackerHasUpdatedValue() {
-        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name", "desc"));
         ConsoleInput input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "0"});
         new StartUI(input, tracker).init();
@@ -66,7 +64,6 @@ public class StartUITest {
 
     @Test
     public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name", "desc"));
         Item item1 = tracker.add(new Item("test1", "desc"));
         ConsoleInput input = new StubInput(new String[]{"3", item.getId(), "Y", "0"});
