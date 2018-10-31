7c70b9486b24d6c2142be5706dd716ae021eecea
Igor Valnev <dx2389@gmail.com>
2018-10-31 22:32:31 +0300

    Added Decorator pattern to Tracker.

--- chapter_002/src/main/java/tracker/ValidateInput.java
+++ chapter_002/src/main/java/tracker/ValidateInput.java
@@ -2,27 +2,32 @@ package tracker;
 
 import java.util.List;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private Input input;
+
+    public ValidateInput(Input input) {
+        this.input = input;
+    }
 
     @Override
     public String ask(String question) {
-        return super.ask(question);
+        return this.input.ask(question);
     }
 
     @Override
     public int ask(String question, List<Integer> range) {
         int value = -1;
-        boolean valid = true;
+        boolean invalid = true;
         do {
             try {
-                value = super.ask(question, range);
-                valid = false;
+                value = this.input.ask(question, range);
+                invalid = false;
             } catch (MenuOutException e) {
                 System.out.println("Нет такого пункта, выберите другой.");
             } catch (NumberFormatException e) {
                 System.out.println("Нужно вводить только цифры. Повторите ввод.");
             }
-        } while (valid);
+        } while (invalid);
         return value;
     }
 }
