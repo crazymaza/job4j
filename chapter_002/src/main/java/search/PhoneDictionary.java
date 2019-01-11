6f7d30fefa4a302e6cbc1e326c1367c7aaf3c38c
Igor Valnev <dx2389@gmail.com>
2019-01-11 22:57:42 +0300

    Сделал поиск пользователя в телефонном справочнике.

---
+++ chapter_002/src/main/java/search/PhoneDictionary.java
@@ -0,0 +1,28 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getSurname().contains(key) || person.getAddress().contains(key) ||
+            person.getName().contains(key) || person.getPhone().contains(key)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
