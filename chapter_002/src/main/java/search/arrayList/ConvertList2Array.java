c428cb28537e96768a389627e288a6d0effe7385
Igor Valnev <dx2389@gmail.com>
2019-01-20 21:20:22 +0300

    Перевод ArrayList в двумерный массив. Не доделал.

---
+++ chapter_002/src/main/java/search/arrayList/ConvertList2Array.java
@@ -0,0 +1,37 @@
+package search.arrayList;
+
+import java.util.List;
+
+/**
+ * Внутри методов использовать foreach.
+ * <p>
+ * public int[][] toArray (List<Integer> list, int rows) {} - метод toArray должен
+ * равномерно разбить лист на количество строк двумерного массива.
+ * В методе toArray должна быть проверка - если количество элементов
+ * не кратно количеству строк - оставшиеся значения в массиве заполнять нулями.
+ * <p>
+ * Например в результате конвертации листа со значениями (1,2,3,4,5,6,7)
+ * с разбиением на 3 строки должен получиться двумерный массив {{1, 2, 3} {4, 5, 6} {7, 0 ,0}}
+ */
+public class ConvertList2Array {
+    public int[][] toArray(List<Integer> list, int rows) {
+        int cells;
+        if (list.size() % rows != 0) {
+            cells = (list.size() / rows) + 1;
+        } else {
+            cells = list.size() / rows;
+        }
+        int[][] array = new int[rows][cells];
+        for (int[] i : array) {
+            int count = 0;
+            for (int t : list) {
+                if (count != cells) {
+                    i[count++] = t;
+                } else {
+                    break;
+                }
+            }
+        }
+        return array;
+    }
+}
