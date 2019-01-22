8a79a8eabb6d46465c0f91aed50d5b954b4c9049
Igor Valnev <dx2389@gmail.com>
2019-01-22 22:22:25 +0300

    Переделал очередь с приоритетом.

--- chapter_002/src/main/java/search/linkedList/PriorityQueue.java
+++ chapter_002/src/main/java/search/linkedList/PriorityQueue.java
@@ -9,17 +9,25 @@ public class PriorityQueue {
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определяется по полю приоритет.
      * Для вставик использовать add(int index, E value)
+     * <p>
+     * {value}index - индекс элемента в массиве.
+     * Проходимся по всему массиву {value}tasks и сравниваем приоритет
+     * всех элементов коллекции.
+     * Если элемент не null и если приоритет элемента больще приоритета {value}task,
+     * то тогда мы выходим из цикла и итерируем {value}index.
+     * В противном случае мы записываем элемент в коллекцию под значением {value}index.
      *
      * @param task задача
      */
     public void put(Task task) {
-        if (tasks.isEmpty()) {
-            tasks.add(task);
+        int index = 0;
+        for (Task t : tasks) {
+            if (t != null && t.getPriority() > task.getPriority()) {
+                break;
+            }
+            index++;
         }
-        if (task.getPriority() < tasks.getFirst().getPriority()
-                && tasks.getFirst() != null) {
-            tasks.addFirst(task);
-        }
+        this.tasks.add(index, task);
     }
 
     public Task take() {
