cb096464496e676a24fb12ac3a5696bf85c8508e
Igor Valnev <dx2389@gmail.com>
2019-01-24 21:25:03 +0300

    Переделал массив в лист.

--- chapter_002/src/main/java/search/arrayList/ConvertMatrix2List.java
+++ chapter_002/src/main/java/search/arrayList/ConvertMatrix2List.java
@@ -6,19 +6,11 @@ import java.util.List;
 public class ConvertMatrix2List {
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
-        int indexOfList = 0;
         for (int[] a : array) {
-            int count = 0;
             for (int t : a) {
-                if (count != a.length) {
-                    list.add(indexOfList++, t);
-                    count++;
-                } else {
-                    break;
-                }
+                list.add(t);
             }
         }
-
         return list;
     }
 }
