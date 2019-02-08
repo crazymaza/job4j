a684b7f136e400971a131930380493193b7c731e
Igor Valnev <dx2389@gmail.com>
2019-02-08 23:12:51 +0300

    Пишу свой метод compare

---
+++ chapter_002/src/main/java/search/ListCompare.java
@@ -0,0 +1,18 @@
+package search;
+
+import java.util.Comparator;
+
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int min = Math.min(left.length(),right.length());
+        int result = 0;
+        for (int index = 0; index < min; index++) {
+             result = Character.compare(left.charAt(index),right.charAt(index));
+        }
+        if (result == 0) {
+            result = Integer.compare(left.length(),right.length());
+        }
+        return result;
+    }
+}
