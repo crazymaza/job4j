3a23e54f854a9acac7a8372c40328589ce71626a
Igor Valnev <dx2389@gmail.com>
2019-03-10 20:08:14 +0300

    Тестирование.

--- chapter_002/src/test/java/search/ConvertList2ArrayTest.java
+++ chapter_002/src/test/java/search/ConvertList2ArrayTest.java
@@ -1,7 +1,7 @@
 package search;
 
 import org.junit.Test;
-import search.arrayList.ConvertList2Array;
+import search.arraylist.ConvertList2Array;
 
 import java.util.Arrays;
 
@@ -37,4 +37,20 @@ public class ConvertList2ArrayTest {
         };
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void when8ElementsThen88() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8),
+                2
+        );
+        int[][] expect = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8}
+        };
+        assertThat(result, is(expect));
+    }
+
+
 }
